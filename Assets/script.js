a372a11d1420e1073006f23abd4d41286eedf5e4
Chris Newbold <104353732+ChrisNewbold@users.noreply.github.com>
2022-06-13 02:46:31 +1000

    jQuery

--- Assets/script.js
+++ Assets/script.js
@@ -1,22 +1,22 @@
 // const d = new Date();
-document.getElementById("currentDay").innerHTML = moment().format('dddd, MMMM Do')
+$("#currentDay").html(moment().format('dddd, MMMM Do'));
 
 const currentHour = moment().hour(); // Number
 
 // this determins the loop staying between the set hours of 9am and 5pm and assigns the class that determins the colour it is highlighted based on a time of day
 for (let i = 9; i <= 17; i++) {
-    document.getElementById(`hour${i}input`).innerHTML = localStorage.getItem(`hour${i}input`)
-    document.getElementById(`hour${i}`).addEventListener('submit', function (event) {
+    $(`#hour${i}input`).html(localStorage.getItem(`hour${i}input`))
+    $(`#hour${i}`).on('submit', function (event) {
         event.preventDefault()
         localStorage.setItem(`hour${i}input`, event.target.elements.userinput.value);
     })
     if (i < currentHour) {
-        document.getElementById(`hour${i}input`).classList.add('past')
+        $(`#hour${i}input`).addClass('past')
     }
     if (i === currentHour) {
-        document.getElementById(`hour${i}input`).classList.add('present')
+        $(`#hour${i}input`).addClass('present')
     }
     if (i > currentHour) {
-        document.getElementById(`hour${i}input`).classList.add('future')
+        $(`#hour${i}input`).addClass('future')
     }
 }
